f960f53b48edc9fd5598bd515a47d28dfb301d74
Scott McNeill <scottj_mcneill@rapid7.com>
2024-07-18 09:50:13 +0100

    playing with styles

--- lua/joeconfig/lazy_plugins/theme.lua
+++ lua/joeconfig/lazy_plugins/theme.lua
@@ -1,10 +1,38 @@
 return {
+    -- {
+    --     'rose-pine/neovim',
+    --     priority = 1000, -- Make sure to load this before all the other start plugins.
+    --     init = function()
+    --         vim.cmd.colorscheme 'rose-pine'
+    --         vim.cmd.hi 'Comment gui=none'
+    --     end,
+    -- },
+
+    -- {
+    --     'morhetz/gruvbox',
+    --     priority = 1000, -- Make sure to load this before all the other start plugins.
+    --     init = function()
+    --         vim.cmd.colorscheme 'gruvbox'
+    --         vim.cmd.hi 'Comment gui=none'
+    --     end,
+    -- },
+
+    -- {
+    --     'navarasu/onedark.nvim',
+    --     priority = 1000, -- Make sure to load this before all the other start plugins.
+    --     init = function()
+    --         vim.cmd.colorscheme 'onedark'
+    --         vim.cmd.hi 'Comment gui=none'
+    --     end,
+    -- },
+
     {
-        'rose-pine/neovim',
+        'rebelot/kanagawa.nvim',
         priority = 1000, -- Make sure to load this before all the other start plugins.
         init = function()
-            vim.cmd.colorscheme 'rose-pine'
+            vim.cmd.colorscheme 'kanagawa'
             vim.cmd.hi 'Comment gui=none'
         end,
     },
+
 }
