0bc02c78e104e032d2d0aa637721c618405cc9b1
Scott McNeill <scottj_mcneill@rapid7.com>
2025-02-07 16:20:17 +0000

    adding figitive for git blame for nice reasons i swear

--- lua/joeconfig/plugins/theme.lua
+++ lua/joeconfig/plugins/theme.lua
@@ -1,21 +1,21 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        init = function ()
-            vim.cmd.colorscheme("tokyonight-moon")
-        end,
-        opts = {},
-    }
     -- {
-    --     'rose-pine/neovim',
-    --     priority = 1000, -- Make sure to load this before all the other start plugins.
-    --     init = function()
-    --         vim.cmd.colorscheme 'rose-pine'
-    --         vim.cmd.hi 'Comment gui=none'
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     init = function ()
+    --         vim.cmd.colorscheme("tokyonight-moon")
     --     end,
-    -- },
+    --     opts = {},
+    -- }
+    {
+        'rose-pine/neovim',
+        priority = 1000, -- Make sure to load this before all the other start plugins.
+        init = function()
+            vim.cmd.colorscheme 'rose-pine'
+            vim.cmd.hi 'Comment gui=none'
+        end,
+    },
 
     -- {
     --     'morhetz/gruvbox',
