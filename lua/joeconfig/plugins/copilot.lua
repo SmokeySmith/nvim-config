6fea71e2aeb7cce5806964093eac907387770e4c
Scott McNeill <scottj_mcneill@rapid7.com>
2025-06-06 16:44:15 +0100

    move stuff around for no reason

--- lua/joeconfig/plugins/copilot.lua
+++ lua/joeconfig/plugins/copilot.lua
@@ -1,12 +1,11 @@
 return {
-    -- enable at work
-  -- {
-  --   'github/copilot.vim'
-  -- },
+  {
+    -- i've disabled the autocompletion because i find it very annoying but i need it for chat
+    'github/copilot.vim'
+  },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      { "github/copilot.vim" },
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken", -- Only on MacOS or Linux
