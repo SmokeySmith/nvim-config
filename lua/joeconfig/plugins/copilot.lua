0ba3603e7429e3db3c2bfc818d20def88a86e247
Scott McNeill <scottj_mcneill@rapid7.com>
2025-06-06 12:06:31 +0100

    added keybind

--- lua/joeconfig/plugins/copilot.lua
+++ lua/joeconfig/plugins/copilot.lua
@@ -16,8 +16,18 @@ return {
           normal = '<C-r>',
           insert = '<C-r>'
         }
-      }
+      },
       -- See Configuration section for options
+    },
+    keys = {
+        {
+            '<leader>cc',
+            function()
+              vim.cmd("CopilotChat")
+            end,
+            mode = '',
+            desc = '[C]opilot [C]hat',
+        },
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
