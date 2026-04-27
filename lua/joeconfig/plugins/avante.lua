2b35c68a1e15d2a9229bf9dd6154481203e7b841
sjmcneill-r7 <scottj_mcneill@rapid7.com>
2026-04-27 09:52:28 +0100

    few work changes

--- lua/joeconfig/plugins/avante.lua
+++ lua/joeconfig/plugins/avante.lua
@@ -11,16 +11,25 @@ return {
 		-- for example
 		provider = "claude",
 		providers = {
-			claude = {
-				endpoint = "https://api.anthropic.com",
-				model = "claude-sonnet-4-20250514",
-				timeout = 30000, -- Timeout in milliseconds
+            copilot = {
+                model = "claude-sonnet-4",
+                timeout = 30000, -- Timeout in milliseconds
 				extra_request_body = {
 					temperature = 0.75,
 					max_tokens = 20480,
 				},
-			},
+            },
+			-- claude = {
+			-- 	endpoint = "https://api.anthropic.com",
+			-- 	model = "claude-sonnet-4-20250514",
+			-- 	timeout = 30000, -- Timeout in milliseconds
+			-- 	extra_request_body = {
+			-- 		temperature = 0.75,
+			-- 		max_tokens = 20480,
+			-- 	},
+			-- },
 		},
+
 	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
--- lua/joeconfig/plugins/copilot.lua
+++ lua/joeconfig/plugins/copilot.lua
@@ -2,37 +2,37 @@ return {
   {
     'github/copilot.vim'
   },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-    },
-    build = "make tiktoken",                          -- Only on MacOS or Linux
-    opts = {
-      model = 'gpt-4.1',                              -- AI model to use
-      temperature = 0.1,                              -- Lower = focused, higher = creative
-      window = {
-        layout = 'vertical',                          -- 'vertical', 'horizontal', 'float'
-        width = 0.5,                                  -- 50% of screen width
-      },
-      mappings = {
-        reset = {
-          normal = '<C-r>',
-          insert = '<C-r>'
-        }
-      },
-      -- See Configuration section for options
-    },
-    keys = {
-      {
-        '<leader>cc',
-        function()
-          vim.cmd("CopilotChat")
-        end,
-        mode = '',
-        desc = '[C]opilot [C]hat',
-      },
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   dependencies = {
+  --     { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+  --   },
+  --   build = "make tiktoken",                          -- Only on MacOS or Linux
+  --   opts = {
+  --     model = 'gpt-4.1',                              -- AI model to use
+  --     temperature = 0.1,                              -- Lower = focused, higher = creative
+  --     window = {
+  --       layout = 'vertical',                          -- 'vertical', 'horizontal', 'float'
+  --       width = 0.5,                                  -- 50% of screen width
+  --     },
+  --     mappings = {
+  --       reset = {
+  --         normal = '<C-r>',
+  --         insert = '<C-r>'
+  --       }
+  --     },
+  --     -- See Configuration section for options
+  --   },
+  --   keys = {
+  --     {
+  --       '<leader>cc',
+  --       function()
+  --         vim.cmd("CopilotChat")
+  --       end,
+  --       mode = '',
+  --       desc = '[C]opilot [C]hat',
+  --     },
+  --   },
+  --   -- See Commands section for default commands if you want to lazy load on them
+  -- },
 }
