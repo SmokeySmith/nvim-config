5056e4e10720bced00eba309ada10efbaadf1ce0
joe <smokeyroll20@gmail.com>
2026-09-17 22:39:30 +0100

    add rustowl

---
+++ lua/basic_andy/plugins/rustowl.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"cordx56/rustowl",
+		version = "0.4.0", -- Latest stable version
+		build = "cargo install rustowl",
+		lazy = false, -- This plugin is already lazy
+		opts = {},
+	},
+}
